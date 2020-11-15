607ddf3b666a52ae54ffbe3b1119bc3fd8250835
Dimitris Lamprinos <pkakelas@gmail.com>
2020-11-16 01:56:31 +0200

    Add basic schema

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE crawled_document (
+    id TEXT PRIMARY KEY NOT NULL,
+    url TEXT NOT NULl,
+    title TEXT NOT NULL,
+    neighbors TEXT NOT NULL,
+    termfreq TEXT NOT NULL,
+    UNIQUE(url)
+);
+CREATE TABLE document_frequency (
+    term INTEGER NOT NULL,
+    occurencies TEXT NOT NULL,
+    UNIQUE(term)
+)
