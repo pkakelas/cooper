d0e5658eb3d64e356f9ea77f3e24054081436b92
Dimitris Lamprinos <pkakelas@gmail.com>
2020-11-15 03:54:48 +0200

    Implement extractStems

--- tokenizer.go
+++ tokenizer.go
@@ -1,14 +1,29 @@
 package main
 
-import "github.com/PuerkitoBio/goquery"
+import (
+	"fmt"
+	"strings"
+	"unicode"
 
-func extractText(document *goquery.Document) (urls []string) {
-	htmlTags := "h1, h2, h3, h3, h5, h6, p, li, a"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/kljensen/snowball"
+)
 
-	document.Find(htmlTags).Each(func(i int, s *goquery.Selection) {
-		text := s.Text()
-		urls = append(urls, text)
-	})
+func extractStems(document *goquery.Document) (parsed []string) {
+	text := extractText(document)
+	words := strings.Fields(text)
+
+	for _, word := range words {
+		text := strings.TrimFunc(word, trimAllButLetters)
+
+		stemmed, err := snowball.Stem(text, "english", true)
+		if err != nil || len(stemmed) == 0 {
+			fmt.Println("[TEXT_PARSER] Word couldn't be parsed:", word, err)
+			continue
+		}
+
+		parsed = append(parsed, stemmed)
+	}
 
 	return
 }
@@ -21,4 +36,18 @@ func extractLinks(document *goquery.Document) (urls []string) {
 	})
 
 	return
+}
+
+func extractText(document *goquery.Document) (text string) {
+	htmlTags := "h1, h2, h3, h3, h5, h6, p, li, a"
+
+	document.Find(htmlTags).Each(func(i int, s *goquery.Selection) {
+		text += " " + s.Text()
+	})
+
+	return
+}
+
+func trimAllButLetters(r rune) bool {
+	return !unicode.IsLetter(r)
 }
